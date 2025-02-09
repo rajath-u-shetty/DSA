4e1b90fe13d50b581e34d73ca8c09c73ecd6945e
rajath U Shetty <rajathushetty56@gmail.com>
2025-02-10 00:03:45 +0530

    Arr-Pblms-2

--- code/3.Arrays/problems/BombTheBase.cpp
+++ code/3.Arrays/problems/BombTheBase.cpp
@@ -28,3 +28,230 @@ int main() {
     }
     return 0;
 }
+
+
+
+// Bomb the base
+// In Chefland, there are 
+// N
+// N houses numbered from 
+// 1
+// 1 to 
+// N
+// N, 
+// i
+// t
+// h
+// i 
+// th
+//   house has a defence system having strength 
+// A
+// i
+// A 
+// i
+// ​
+//  .
+//
+// Chef suspects a bomb drop on one of the houses very soon. A bomb with attack strength 
+// X
+// X can destroy the 
+// i
+// t
+// h
+// i 
+// th
+//   house, if the defence system of the 
+// i
+// t
+// h
+// i 
+// th
+//   house 
+// A
+// i
+// A 
+// i
+// ​
+//  , is strictly less than 
+// X
+// X.
+//
+// Also, when the 
+// i
+// t
+// h
+// i 
+// th
+//   house is destroyed due to the bomb, all houses with indices 
+// j
+// j such that 
+// 1
+// ≤
+// j
+// <
+// i
+// 1≤j<i get destroyed as well irrespective of their defence system.
+//
+// Given one bomb with attack strength 
+// X
+// X, find the maximum number of houses that can get destroyed.
+//
+// Input Format
+// The first line will contain 
+// T
+// T - the number of test cases. Then the test cases follow.
+// First line of each test case contains 
+// 2
+// 2 integers 
+// N
+// ,
+// X
+// N,X.
+// Second line of test case contains 
+// N
+// N space separated integers 
+// A
+// 1
+// ,
+// A
+// 2
+// ,
+// …
+// ,
+// A
+// N
+// A 
+// 1
+// ​
+//  ,A 
+// 2
+// ​
+//  ,…,A 
+// N
+// ​
+//  .
+// Output Format
+// For each test case, output in a single line the maximum number of houses that can get destroyed if the bomb can hit any house.
+//
+// Constraints
+// 1
+// ≤
+// T
+// ≤
+// 100
+// 1≤T≤100
+// 1
+// ≤
+// N
+// ≤
+// 1
+// 0
+// 5
+// 1≤N≤10 
+// 5
+//  
+// 1
+// ≤
+// X
+// ≤
+// 1
+// 0
+// 9
+// 1≤X≤10 
+// 9
+//  
+// 1
+// ≤
+// A
+// i
+// ≤
+// 1
+// 0
+// 9
+// 1≤A 
+// i
+// ​
+//  ≤10 
+// 9
+//  
+// Sum of 
+// N
+// N over all test cases does not exceed 
+// 1
+// 0
+// 5
+// 10 
+// 5
+//  
+// Sample 1:
+// Input
+// Output
+// 2
+// 8 6
+// 4 1 6 1 6 5 6 8
+// 2 1
+// 3 5
+// 6
+// 0
+// Explanation:
+// Test Case 
+// 1
+// 1: The bomb can only destroy houses 
+// 1
+// ,
+// 2
+// ,
+// 4
+// ,
+// 1,2,4, and 
+// 6
+// 6.
+//
+// If it hits house 
+// 1
+// 1, only house 
+// 1
+// 1 is destroyed.
+// If it hits house 
+// 2
+// 2, houses 
+// 1
+// 1 and 
+// 2
+// 2 are destroyed.
+// If it hits house 
+// 4
+// 4, houses 
+// 1
+// ,
+// 2
+// ,
+// 3
+// 1,2,3 and 
+// 4
+// 4 are destroyed.
+// If it hits house 
+// 6
+// 6, houses 
+// 1
+// ,
+// 2
+// ,
+// 3
+// ,
+// 4
+// ,
+// 5
+// ,
+// 1,2,3,4,5, and 
+// 6
+// 6 are destroyed.
+// The maximum number of destroyed houses is 
+// 6
+// 6.
+//
+// Test Case 
+// 2
+// 2: The bomb cannot destroy any of the houses as the defence system of each house is not lesser than attack power of bomb. Thus, the total number of destroyed houses is 
+// 0
+// 0.
--- code/3.Arrays/problems/SquidGame.cpp
+++ code/3.Arrays/problems/SquidGame.cpp
@@ -58,3 +58,196 @@ int main() {
 //     }
 // }
 //
+// The Squid Game
+// "Let the games begin."
+//
+// Squid Game has become a blockbuster hit and the frontman is now finding it difficult to accommodate all the participants in Squid Game 2.0. So, he decided that he will allow only those participants who could solve the following problem.
+//
+// There are a total of 
+// N
+// N players who are competing in the Squid Game, numbered from 
+// 1
+// 1 to 
+// N
+// N. When the 
+// i
+// th
+// i 
+// th
+//   player gets eliminated from the game, 
+// A
+// i
+// A 
+// i
+// ​
+//   amount of money is added to the prize pool. The game is played until 
+// N
+// −
+// 1
+// N−1 players get eliminated, and the only player left is declared as the winner. The winner gets all the money present in the prize pool.
+//
+// You are given an array 
+// A
+// A consisting of 
+// N
+// N elements, where 
+// A
+// i
+// A 
+// i
+// ​
+//   denotes the prize money added to the prize pool when the 
+// i
+// th
+// i 
+// th
+//   player gets eliminated from the game. Find the maximum prize that the winner can get, given that you can choose any player to be the winner.
+//
+// Input Format
+// The first line of input contains a single integer 
+// T
+// T, denoting the number of test cases. The description of 
+// T
+// T test cases follows.
+// The first line of each test case contains an integer 
+// N
+// N, denoting the number of players.
+// The second line of each test case contains 
+// N
+// N space-separated integers 
+// A
+// 1
+// ,
+// A
+// 2
+// ,
+// …
+// ,
+// A
+// N
+// A 
+// 1
+// ​
+//  ,A 
+// 2
+// ​
+//  ,…,A 
+// N
+// ​
+//  , denoting the amount of money added to the prize pool when the 
+// i
+// th
+// i 
+// th
+//   (
+// 1
+// ≤
+// i
+// ≤
+// N
+// 1≤i≤N) player dies.
+// Output Format
+// For each test case, output in a single line the maximum prize that the winner can get, given that you can choose any player to be the winner.
+//
+// Constraints
+// 1
+// ≤
+// T
+// ≤
+// 1000
+// 1≤T≤1000
+// 2
+// ≤
+// N
+// ≤
+// 1
+// 0
+// 5
+// 2≤N≤10 
+// 5
+//  
+// 0
+// ≤
+// A
+// i
+// ≤
+// 1
+// 0
+// 4
+// 0≤A 
+// i
+// ​
+//  ≤10 
+// 4
+//  
+// The sum of 
+// N
+// N across all test cases does not exceed 
+// 1
+// 0
+// 6
+// 10 
+// 6
+//  .
+// Sample 1:
+// Input
+// Output
+// 3
+// 3
+// 3 1 2
+// 5
+// 1 1 1 1 1
+// 6
+// 3 6 4 2 5 1
+// 5
+// 4
+// 20
+// Explanation:
+// Test Case 1:
+//
+// If we choose the first player to be the winner, he will win the game when the second and third players die. Hence, the amount of money won by him will be 
+// 1
+// +
+// 2
+// =
+// 3
+// 1+2=3.
+// If we choose the second player to be the winner, he will win the game when the first and third players die. Hence, the amount of money won by him will be 
+// 3
+// +
+// 2
+// =
+// 5
+// 3+2=5.
+// If we choose the third player to be the winner, he will win the game when the first and second players die. Hence, the amount of money won by him will be 
+// 3
+// +
+// 1
+// =
+// 4
+// 3+1=4.
+// Therefore, we can clearly see that the maximum amount of money that can be won by any player is 
+// 5
+// 5.
+//
+// Test Case 2: Irrespective of who is chosen, the winner will always win an amount of 
+// 4
+// 4.
+//
+// Test Case 3: If we choose the sixth player to be the winner, the amount won by him will be 
+// 3
+// +
+// 6
+// +
+// 4
+// +
+// 2
+// +
+// 5
+// =
+// 20
+// 3+6+4+2+5=20. It can be proven that if we choose any other player to be the winner, the amount is less than 
+// 20
+// 20. Hence, the maximum amount of money that can be won by any player is 
+// 20
+// 20.
